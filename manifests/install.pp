991612b70ad38a307f58c563db21b7388466feb0
malnick <malnick@gmail..com>
2015-02-09 17:15:04 -0800

    install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,7 @@
 class newrelic_java_agent::install (
 
     $version        = '3.9.0',
-    $install_dir    = '/opt/newrelic/java',
+    $install_dir    = '/opt/newrelic',
     $app_user       = 'newrelic',
     $app_group      = 'newrelic',
 
@@ -10,20 +10,19 @@ class newrelic_java_agent::install (
     $agent_jar      = "/newrelic-agent-${version}.jar"
     $download       = "https://download.newrelic.com/newrelic/java-agent/newrelic-agent/${version}/newrelic-agent-${version}.jar"
     
-    $dirs = ['/opt/newrelic','/opt/newrelic/java']
-    file { $dirs:
+    file { $install_dir: 
         ensure => directory,
         owner  => $appuser,
         group  => $appuser,
     }
 
      exec { "wget_agent": 
-        command => "/usr/bin/wget -P ${install_dir}/newrelic.jar ${download}", 
-        require => File[$dirs],
+        command => "/usr/bin/wget -P ${install_dir}/java ${download}", 
+        require => File[$install_dir],
     }
 
     exec { 'newrelic_install':
-        command => "/usr/bin/java -jar ${install_dir}/newrelic.jar -s ${install_dir} install",
+        command => "/usr/bin/java -jar ${install_dir}/java/newrelic-agent-${version}.jar -s ${install_dir} install",
         require => Exec['wget_agent'],
     }
 }

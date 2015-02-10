4373415a496e8e506b47b0988754c89b3aea1763
malnick <malnick@gmail..com>
2015-02-09 17:40:33 -0800

    yaml

--- manifests/init.pp
+++
@@ -1,9 +0,0 @@
-class newrelic_java_agent (
-    $license_key,   
-){
-
-    class { ::newrelic_java_agent::install: }#before => Class[::newrelic_java_agent::agent] }
-    #class { ::newrelic_java_agent::agent:}
-
-}
-    
---
+++ manifests/yml.pp
@@ -0,0 +1,24 @@
+define newrelic_java_agent::yml (
+
+    $license_key,
+    $enabled                = true,
+    $app_name               = $name,
+    $agent_type             = 'java',
+    $loglevel               = 'info',
+    $audit_mode             = true,
+    $log_file_count         = '1',
+    $log_limit_in_kbytes    = '0',
+    $log_daily              = true,
+    $logfile                = "newrelic_agent",
+    $daemon_ssl             = false,
+    $logfile_path           = false, # default to instal dir
+){
+
+    file { "/opt/newrelic/java/${name}.yml":
+        content => template('newrelic_java_agent/newrelic.yml.erb')
+        mode    => '0644',
+        owner   => 'newrelic',
+        group   => 'newrelic',
+    }
+
+}

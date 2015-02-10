55e11ad3a752584ef7baf137cf953c23604f0617
malnick <malnick@gmail..com>
2015-02-09 16:46:56 -0800

    install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,12 +3,13 @@ class newrelic_java_agent::install (
     $download       = '"https://download.newrelic.com/newrelic/java-agent/newrelic-agent/#{version}/newrelic-agent-#{version}.jar"',
     $version        = '3.9.0',
     $install_dir    = '/opt/newrelic/java'
-    $agent_jar      = "/newrelic-agent-${version}.jar",
     $app_user       = 'newrelic',
     $app_group      = 'newrelic',
 
 
 ) {
+
+    $agent_jar      = "/newrelic-agent-${version}.jar"
 
     $dirs = ['/opt/newrelic','/opt/newrelic/java']
     file { $dirs:
